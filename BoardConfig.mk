425dacd42ee146012d6f13f15c9fa03493e59247
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-03 19:18:28 +0530

    peridot: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/peridot
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# Inherit from proprietary files for miuicamera
+-include device/xiaomi/peridot-miuicamera/BoardConfig.mk
+
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := \
